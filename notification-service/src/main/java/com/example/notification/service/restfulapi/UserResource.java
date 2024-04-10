14dce6b91a4358d73bb52084eb5224c420fc968a
Jinu <jinupm695@gmail.com>
2024-04-10 22:23:16 +0530

    versioningtest

--- notification-service/src/main/java/com/example/notification/service/restfulapi/UserResource.java
+++ notification-service/src/main/java/com/example/notification/service/restfulapi/UserResource.java
@@ -1,21 +1,24 @@
 package com.example.notification.service.restfulapi;
 
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserResource {
     private UserDaoService service;
+    private MessageSource messageSource;
 
-    public UserResource(UserDaoService service){
+    public UserResource(UserDaoService service,MessageSource messageSource){
         this.service = service;
+        this.messageSource=messageSource;
     }
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
@@ -39,5 +42,11 @@ public class UserResource {
     @DeleteMapping("/users/{id}")
     public void deleteUserById(@PathVariable("id") int id){
         service.deleteUserById(id);
+    }
+
+    @GetMapping(path = "/hello-world-internalized")
+    public String helloWorldInternalize(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
     }
 }
